cfb0fadd4728821c7a469e0a2c79e2cce8c6acce
Alun Price <alunprice089@gmail.com>
2023-03-30 04:11:53 +0100

    Added Utils comp to allow for genres

---
+++ src/components/Utils.jsx
@@ -0,0 +1,30 @@
+export const genres = [
+  {
+    id: 1,
+    name: "All",
+  },
+  {
+    id: 28,
+    name: "Action",
+  },
+  {
+    id: 12,
+    name: "Adventure",
+  },
+  {
+    id: 16,
+    name: "Animation",
+  },
+  {
+    id: 35,
+    name: "Comedy",
+  },
+  {
+    id: 99,
+    name: "Documentary",
+  },
+  {
+    id: 878,
+    name: "Science Fiction",
+  },
+];
