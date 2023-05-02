6dc56ff64a6ae2443a7aa9b4af0a225c44f24109
Alun Price <alunprice089@gmail.com>
2023-05-02 19:29:07 +0100

    fix typo in api url

--- functions/movies.js
+++ functions/movies.js
@@ -1,8 +1,8 @@
 import fetch from "node-fetch";
 
-exports.handler = async function (event, context) {
-  const api_key = "6cb58f5c37660b569aaa773b2dabc325";
-  const url = `https://api.themoviedb.org/3/movie/popular/?api_key=${api_key}&language=en-US&page=1`;
+exports.handler = async (event, context) => {
+  const api_key = process.env.VITE_MOVIES_API_KEY;
+  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`;
   console.log("KEY: ".api_key);
   const response = await fetch(url);
   const data = await response.json();
