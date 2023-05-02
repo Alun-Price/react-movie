a618ef212ac07d7c0bd49ebbcd5163dd499dbfa5
Alun Price <alunprice089@gmail.com>
2023-05-02 19:14:52 +0100

    check movie function is working

--- functions/movies.js
+++ functions/movies.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 
 exports.handler = async function (event, context) {
-  const api_key = import.meta.env.VITE_MOVIES_API_KEY;
+  const api_key = "6cb58f5c37660b569aaa773b2dabc325";
   const url = `https://api.themoviedb.org/3/movie/popular/?api_key=${api_key}&language=en-US&page=1`;
   console.log("KEY: ".api_key);
   const response = await fetch(url);
